e69ddad6c58736a852a21ce57336122bc2119aa4
agungfir10 <agungfirid@gmail.com>
2023-01-27 00:54:45 +0700

    fix docker-compose, build_push_image

--- build_push_image.sh
+++ build_push_image.sh
@@ -5,6 +5,6 @@ docker images
 # mengganti nama image dan tag nya
 docker tag item-app:v1 agungfir10/item-app:v1
 # login ke docker hub dengan password yang telah tersimpan di env
-echo $PASSWORD_DOCKER_HUB | docker login -u fikrihelmi17 --password-stdin
+echo $PASSWORD_DOCKER_HUB | docker login -u agungfir10 --password-stdin
 # push atau mengunggah image ke registry docker hub
 docker push agungfir10/item-app:v1
